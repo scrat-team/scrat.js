e65ab6a972c4c38e619aff46aa4205e79c3a9578
switer <guankaishe@gmail.com>
2015-05-03 11:28:30 +0800

    travis-ci debug

--- test/tasks.js
+++ test/tasks.js
@@ -1,8 +1,14 @@
 var childProcess = require("child_process")
 var server = childProcess.spawn("node", ['./test/server'], { stdio: 'inherit' })
 // var testing = childProcess.exec("mocha-phantomjs http://localhost:3001", function (err, stdout, stderr) {
+childProcess.exec("mocha-phantomjs --version", function (err, stdout, stderr) {
+    if (!err) console.log('mocha-phantomjs version ', stdout)
+})
 var testing = childProcess.exec("npm run runner", function (err, stdout, stderr) {
-    if (err) throw new Error(err)
+    if (err) {
+        console.log(err)
+        throw new Error(err)
+    }
     if (stderr) throw new Error(stderr)
     console.log(stdout)
     server.kill()
